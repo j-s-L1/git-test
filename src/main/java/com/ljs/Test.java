d54c39d3dc81f335f7f9a0b95a2249033f4d80c0
Ljs <Ljs@gg.com>
2023-05-31 10:31:41 +0800

    first commit

---
+++ src/main/java/com/ljs/Test.java
@@ -0,0 +1,10 @@
+package com.ljs;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println();
+        System.out.println("冲突");
+    }
+}
